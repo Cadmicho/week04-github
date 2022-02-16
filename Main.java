fe85feb52fe91a7d0ff023e93b198de6eb86e7b5
VilmaBety <96948541+VilmaBety@users.noreply.github.com>
2022-02-16 15:03:47 -0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello, my name is Cadmiel");
+        System.out.println("Hello Cadmiel!, my name is Vilma");
     }
 }
